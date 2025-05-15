06a588983b117547a1768811ab67192bd3c80ea8
TotoEnF5 <totooodev@proton.me>
2025-05-15 10:42:05 +0200

    remove .cache

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include "interpreter.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 int main(int argc, char* argv[]) {
-    Interpreter interpreter("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
+    if (argc < 2) {
+	std::cerr << "no file provided" << std::endl;
+	return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    std::ostringstream sstr;
+    sstr << file.rdbuf();
+    file.close();
+
+    Interpreter interpreter(sstr.str());
     interpreter.run();
+
     return 0;
 }
